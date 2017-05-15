a24e4caf7aa517550d398fbd084a9d8afa38d8aa
Jonathan Perret <jonathan@ut7.fr>
2017-05-15 11:25:55 +0200

    Add sprite, move by tapping

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,18 +1,29 @@
 window.onload = function() {
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
+                             { preload: preload, create: create });
+  var sagittarius;
 
-    function preload () {
+  function preload () {
 
-        game.load.image('logo', 'images/phaser.png');
+    game.load.image('logo', 'images/phaser.png');
+    game.load.spritesheet('tiles', 'images/tiles.png', 32, 32);
 
-    }
+  }
 
-    function create () {
+  function create () {
+    sagittarius = game.add.sprite(game.world.centerX,
+                                  game.world.centerY,
+                                  'tiles',
+                                  80);
+    sagittarius.anchor.setTo(0.5, 0.5);
 
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+    game.input.onTap.add(onTap);
+  }
 
-    }
+  function onTap(pointer, doubleTap) {
+    sagittarius.x = pointer.x;
+    sagittarius.y = pointer.y;
 
+  }
 };
